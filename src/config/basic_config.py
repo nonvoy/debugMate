e484639c99fde3dbfdcb75b32184d2b2983fcf51
Timur Babadjanov <timur.btb@gmail.com>
2026-05-27 19:22:08 +0200

    fix: adjusting names

--- src/config/basic_config.py
+++ src/config/basic_config.py
@@ -7,15 +7,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class CeleryConfig(BaseModel):
     """Configuration for Celery."""
 
-    app_name: str = Field(default="api-client", description="Name of the Celery application")
-    task_name: str = Field(..., description="Name of the Celery task to process events")
+    app_name: str = Field(default="debugmate-worker", description="Name of the Celery application")
+    task_name: str = Field(default="analyze_events", description="Name of the Celery task to process events")
     broker_url: str = Field(..., description="URL of the message broker")
     endpoint_url: str | None = Field(default=None, description="Custom endpoint URL for the broker")
     is_secure: bool | None = Field(default=None, description="Use SSL when connecting to the broker")
     region: str = Field(default="eu-central-1", description="AWS region for the broker")
     visibility_timeout: int = Field(default=3600, description="Visibility timeout for tasks in seconds")
     polling_interval: int = Field(default=5, description="Polling interval for the broker in seconds")
-    queue_name: str = Field(default="debugmate-tasks", description="Name of the Celery task queue")
+    queue_name: str = Field(default="debugmate-queue", description="Name of the Celery task queue")
 
 
 class BasicConfig(BaseSettings):
